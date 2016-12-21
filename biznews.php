d444f4aafaf0dae6e4971a2f077160f3c579e3e2
Khushboo Sharma <ksharma13121992@gmail.com>
2016-12-21 18:24:32 +0530

    people custom post type

--- biznews.php
+++ biznews.php
@@ -29,25 +29,76 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-// Required files for registering the post type and taxonomies.
-require plugin_dir_path( __FILE__ ) . 'includes/biznews-post-type.php';
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-biznews-activator.php
+ */
+function activate_biznews() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-biznews-activator.php';
+	Biznews_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-biznews-deactivator.php
+ */
+function deactivate_biznews() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-biznews-deactivator.php';
+	Biznews_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_biznews' );
+register_deactivation_hook( __FILE__, 'deactivate_biznews' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-biznews.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_biznews() {
+
+	$plugin = new Biznews();
+	$plugin->run();
+
+}
+run_biznews();
 
 /****************************************
 **********Companies post type*************
 ****************************************/
-require plugin_dir_path( __FILE__ ) . 'includes/companies/companies-post-type-registrations.php';
-require plugin_dir_path( __FILE__ ) . 'includes/companies/companies-post-type-metaboxes.php';
+// Required files for registering the post type and taxonomies.
+require plugin_dir_path( __FILE__ ) . 'includes/companies/company-class-post-type.php';
+require plugin_dir_path( __FILE__ ) . 'includes/companies/company-class-post-type-registrations.php';
+require plugin_dir_path( __FILE__ ) . 'includes/companies/company-class-post-type-metaboxes.php';
+
 // Instantiate registration class, so we can add it as a dependency to main plugin class.
-$companies_post_type_registrations = new Companies_Post_Type_Registrations;
+$post_type_registrations = new Companies_Post_Type_Registrations;
+
 // Instantiate main plugin file, so activation callback does not need to be static.
-$companies_post_type = new Biznews_Post_Type( $companies_post_type_registrations );
+$post_type = new Company_Post_Type( $post_type_registrations );
+
 // Register callback that is fired when the plugin is activated.
-register_activation_hook( __FILE__, array( $companies_post_type, 'activate' ) );
+register_activation_hook( __FILE__, array( $post_type, 'activate' ) );
+
 // Initialize registrations for post-activation requests.
-$companies_post_type_registrations->init();
+$post_type_registrations->init();
+
 // Initialize metaboxes
-$companies_post_type_metaboxes = new Companies_Post_Type_Metaboxes;
-$companies_post_type_metaboxes->init();
+$post_type_metaboxes = new Companies_Post_Type_Metaboxes;
+$post_type_metaboxes->init();
+
+
 /**
  * Adds styling to the dashboard for the post type and adds investment posts
  * to the "At a Glance" metabox.
@@ -56,22 +107,25 @@ if ( is_admin() ) {
 
 	// Loads for users viewing the WordPress dashboard
 	if ( ! class_exists( 'Dashboard_Glancer' ) ) {
-		require plugin_dir_path( __FILE__ ) . 'includes/biznews-dashboard-glancer.php';  // WP 3.8
+		require plugin_dir_path( __FILE__ ) . 'includes/companies/company-class-dashboard-glancer.php';  // WP 3.8
 	}
-	require plugin_dir_path( __FILE__ ) . 'includes/companies/companies-post-type-admin.php';
-	$companies_post_type_admin = new Companies_Post_Type_Admin( $companies_post_type_registrations );
-	$companies_post_type_admin->init();
-}
 
+	require plugin_dir_path( __FILE__ ) . 'includes/companies/company-class-post-type-admin.php';
+
+	$post_type_admin = new Companies_Post_Type_Admin( $post_type_registrations );
+	$post_type_admin->init();
+
+}
 /****************************************
-**********Projects post type*************
+**********rojects post type*************
 ****************************************/
-require plugin_dir_path( __FILE__ ) . 'includes/projects/project-post-type-registrations.php';
-require plugin_dir_path( __FILE__ ) . 'includes/projects/project-post-type-metaboxes.php';
+require plugin_dir_path( __FILE__ ) . 'includes/project/project-class-post-type.php';
+require plugin_dir_path( __FILE__ ) . 'includes/project/project-class-post-type-registrations.php';
+require plugin_dir_path( __FILE__ ) . 'includes/project/project-class-post-type-metaboxes.php';
 // Instantiate registration class, so we can add it as a dependency to main plugin class.
 $project_post_type_registrations = new Project_Post_Type_Registrations;
 // Instantiate main plugin file, so activation callback does not need to be static.
-$project_post_type = new Biznews_Post_Type( $project_post_type_registrations );
+$project_post_type = new Project_Post_Type( $project_post_type_registrations );
 // Register callback that is fired when the plugin is activated.
 register_activation_hook( __FILE__, array( $project_post_type, 'activate' ) );
 // Initialize registrations for post-activation requests.
@@ -84,7 +138,57 @@ $project_post_type_metaboxes->init();
  * to the "At a Glance" metabox.
  */
 if ( is_admin() ) {
-	require plugin_dir_path( __FILE__ ) . 'includes/projects/project-post-type-admin.php';
-	$project_post_type_admin = new Project_Post_Type_Admin( $project_post_type_registrations );
-	$project_post_type_admin->init();
+
+	// Loads for users viewing the WordPress dashboard
+	if ( ! class_exists( 'Dashboard_Glancer' ) ) {
+		require plugin_dir_path( __FILE__ ) . 'includes/project/project-class-dashboard-glancer.php';  // WP 3.8
+	}
+
+	require plugin_dir_path( __FILE__ ) . 'includes/project/project-class-post-type-admin.php';
+
+	$post_type_admin = new Project_Post_Type_Admin( $post_type_registrations );
+	$post_type_admin->init();
+
+}
+/****************************************
+**********People post type*************
+****************************************/
+// Required files for registering the post type and taxonomies.
+require plugin_dir_path( __FILE__ ) . 'includes/people/people-class-post-type.php';
+require plugin_dir_path( __FILE__ ) . 'includes/people/people-class-post-type-registrations.php';
+require plugin_dir_path( __FILE__ ) . 'includes/people/people-class-post-type-metaboxes.php';
+
+// Instantiate registration class, so we can add it as a dependency to main plugin class.
+$people_post_type_registrations = new People_Post_Type_Registrations;
+
+// Instantiate main plugin file, so activation callback does not need to be static.
+$people_post_type = new People_Post_Type( $people_post_type_registrations );
+
+// Register callback that is fired when the plugin is activated.
+register_activation_hook( __FILE__, array( $people_post_type, 'activate' ) );
+
+// Initialize registrations for post-activation requests.
+$people_post_type_registrations->init();
+
+// Initialize metaboxes
+$people_post_type_metaboxes = new People_Post_Type_Metaboxes;
+$people_post_type_metaboxes->init();
+
+
+/**
+ * Adds styling to the dashboard for the post type and adds investment posts
+ * to the "At a Glance" metabox.
+ */
+if ( is_admin() ) {
+
+	// Loads for users viewing the WordPress dashboard
+	if ( ! class_exists( 'Dashboard_Glancer' ) ) {
+		require plugin_dir_path( __FILE__ ) . 'includes/people/people-class-dashboard-glancer.php';  // WP 3.8
+	}
+
+	require plugin_dir_path( __FILE__ ) . 'includes/people/people-class-post-type-admin.php';
+
+	$people_post_type_admin = new Companies_Post_Type_Admin( $people_post_type_registrations );
+	$people_post_type_admin->init();
+
 }
--- public/class-biznews-public.php
+++ public/class-biznews-public.php
@@ -1,26 +1,26 @@
 <?php
 
 /**
- * The admin-specific functionality of the plugin.
+ * The public-facing functionality of the plugin.
  *
  * @link       http://atidiv.com
  * @since      1.0.0
  *
  * @package    Biznews
- * @subpackage Biznews/admin
+ * @subpackage Biznews/public
  */
 
 /**
- * The admin-specific functionality of the plugin.
+ * The public-facing functionality of the plugin.
  *
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the admin-specific stylesheet and JavaScript.
  *
  * @package    Biznews
- * @subpackage Biznews/admin
+ * @subpackage Biznews/public
  * @author     Atidiv <hello@atidiv.com>
  */
-class Biznews_Admin {
+class Biznews_Public {
 
 	/**
 	 * The ID of this plugin.
@@ -44,7 +44,7 @@ class Biznews_Admin {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $plugin_name       The name of the plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
@@ -55,7 +55,7 @@ class Biznews_Admin {
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
+	 * Register the stylesheets for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
 	 */
@@ -73,12 +73,12 @@ class Biznews_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/biznews-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/biznews-public.css', array(), $this->version, 'all' );
 
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
+	 * Register the JavaScript for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
 	 */
@@ -96,7 +96,7 @@ class Biznews_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/biznews-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/biznews-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
